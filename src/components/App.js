65c5d2c13739ca209d8bd43a8aa6328435de4530
michaelyoussef396 <mihcaelyoussef396@gmail.com>
2024-07-13 12:17:44 +1000

    completed all tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
-
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const onChangeType = (type) => {
+    setFilters({ type });
+  };
+
+  const onFindPetsClick = () => {
+    let url = "http://localhost:3001/pets";
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`;
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setPets(data));
+  };
+
+  const onAdoptPet = (id) => {
+    setPets((pets) =>
+      pets.map((pet) =>
+        pet.id === id ? { ...pet, isAdopted: true } : pet
+      )
+    );
+  };
 
   return (
     <div className="ui container">
@@ -15,10 +36,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,20 +1,23 @@
 import React from "react";
 
-function Filters() {
+function Filters({ onChangeType, onFindPetsClick }) {
+  const handleTypeChange = (event) => {
+    onChangeType(event.target.value);
+  };
+
   return (
     <div className="ui form">
       <h3>Animal type</h3>
       <div className="field">
-        <select name="type" id="type" aria-label="type">
+        <select name="type" id="type" aria-label="type" onChange={handleTypeChange}>
           <option value="all">All</option>
           <option value="cat">Cats</option>
           <option value="dog">Dogs</option>
           <option value="micropig">Micropigs</option>
         </select>
       </div>
-
       <div className="field">
-        <button className="ui secondary button">Find pets</button>
+        <button className="ui secondary button" onClick={onFindPetsClick}>Find pets</button>
       </div>
     </div>
   );
